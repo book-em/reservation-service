11ea6ee513a57f8514042aaeed7078704d36d630
magley <60552482+magley@users.noreply.github.com>
2025-08-20 15:42:09 +0200

    feat: Add `user` and `room` client code
    User code is copied from room-service, while room code is a stub

---
+++ src/client/roomclient/model.go
@@ -0,0 +1,92 @@
+package roomclient
+
+import "time"
+
+type RoomDTO struct {
+	ID          uint     `json:"id"`
+	HostID      uint     `json:"hostID"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Address     string   `json:"address"`
+	MinGuests   uint     `json:"minGuests"`
+	MaxGuests   uint     `json:"maxGuests"`
+	Photos      []string `json:"photos"`
+	Commodities []string `json:"commodities"`
+}
+
+type CreateRoomDTO struct {
+	HostID        uint     `json:"hostID"`
+	Name          string   `json:"name"`
+	Description   string   `json:"description"`
+	Address       string   `json:"address"`
+	MinGuests     uint     `json:"minGuests"`
+	MaxGuests     uint     `json:"maxGuests"`
+	PhotosPayload []string `json:"photosPayload"`
+	Commodities   []string `json:"commodities"`
+}
+
+// ---------------------------------------------------------------
+
+type CreateRoomAvailabilityListDTO struct {
+	RoomID uint                            `json:"roomId"`
+	Items  []CreateRoomAvailabilityItemDTO `json:"items"`
+}
+
+type RoomAvailabilityListDTO struct {
+	ID            uint                      `json:"id"`
+	RoomID        uint                      `json:"roomId"`
+	EffectiveFrom time.Time                 `json:"effectiveFrom"`
+	Items         []RoomAvailabilityItemDTO `json:"items"`
+}
+
+type RoomAvailabilityItemDTO struct {
+	ID        uint      `json:"id"`
+	DateFrom  time.Time `json:"dateFrom"`
+	DateTo    time.Time `json:"dateTo"`
+	Available bool      `json:"available"`
+}
+
+type CreateRoomAvailabilityItemDTO struct {
+	// ExistingID is either the ID of an RoomAvailabilityItem that already
+	// exists, or 0 if this is a new item. When 0, a new one will be created in
+	// the DB. When not 0, it will reuse the existing object.
+	ExistingID uint      `json:"existingId"`
+	DateFrom   time.Time `json:"dateFrom"`
+	DateTo     time.Time `json:"dateTo"`
+	Available  bool      `json:"available"`
+}
+
+// ---------------------------------------------------------------
+
+type CreateRoomPriceListDTO struct {
+	RoomID    uint                     `json:"roomId"`
+	Items     []CreateRoomPriceItemDTO `json:"items"`
+	BasePrice uint                     `json:"basePrice"`
+	PerGuest  bool                     `json:"perGuest"`
+}
+
+type RoomPriceListDTO struct {
+	ID            uint               `json:"id"`
+	RoomID        uint               `json:"roomId"`
+	EffectiveFrom time.Time          `json:"effectiveFrom"`
+	BasePrice     uint               `json:"basePrice"`
+	Items         []RoomPriceItemDTO `json:"items"`
+	PerGuest      bool               `json:"perGuest"`
+}
+
+type RoomPriceItemDTO struct {
+	ID       uint      `json:"id"`
+	DateFrom time.Time `json:"dateFrom"`
+	DateTo   time.Time `json:"dateTo"`
+	Price    uint      `json:"price"`
+}
+
+type CreateRoomPriceItemDTO struct {
+	// ExistingID is either the ID of an RoomPriceItem that already
+	// exists, or 0 if this is a new item. When 0, a new one will be created in
+	// the DB. When not 0, it will reuse the existing object.
+	ExistingID uint      `json:"existingId"`
+	DateFrom   time.Time `json:"dateFrom"`
+	DateTo     time.Time `json:"dateTo"`
+	Price      uint      `json:"price"`
+}
