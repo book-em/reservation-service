ae9c6c7fc1c05dbe2a5046716c8df3ee784916df
magley <60552482+magley@users.noreply.github.com>
2025-08-21 15:02:01 +0200

    feat: Create model and DTO for ReservationRequest

--- src/internal/dto.go
+++ src/internal/dto.go
@@ -1,7 +1,56 @@
 package internal
 
+import "time"
+
+type CreateReservationRequestDTO struct {
+	RoomID     uint      `json:"roomId"`
+	DateFrom   time.Time `json:"dateFrom"`
+	DateTo     time.Time `json:"dateTo"`
+	GuestCount uint      `json:"guestCount"`
+}
+
+type ReservationRequestDTO struct {
+	ID         uint      `json:"id"`
+	RoomID     uint      `json:"roomId"`
+	DateFrom   time.Time `json:"dateFrom"`
+	DateTo     time.Time `json:"dateTo"`
+	GuestCount uint      `json:"guestCount"`
+	GuestID    uint      `json:"guestId"`
+	Status     string    `json:"status"`
+	Cost       uint      `json:"cost"`
+}
+
 type ReservationDTO struct {
-	ID      uint `json:"id"`
-	RoomID  uint `json:"roomId"`
-	GuestID uint `json:"guestId"`
+	ID         uint      `json:"id"`
+	RoomID     uint      `json:"roomId"`
+	DateFrom   time.Time `json:"dateFrom"`
+	DateTo     time.Time `json:"dateTo"`
+	GuestCount uint      `json:"guestCount"`
+	GuestID    uint      `json:"guestId"`
+	Cancelled  bool      `json:"cancelled"`
+	Cost       uint      `json:"cost"`
+}
+
+func ToReservationRequestDTO(r ReservationRequest) ReservationRequestDTO {
+	return ReservationRequestDTO{
+		ID:         r.ID,
+		RoomID:     r.RoomID,
+		DateFrom:   r.DateFrom,
+		DateTo:     r.DateTo,
+		GuestCount: r.GuestCount,
+		GuestID:    r.GuestID,
+		Status:     string(r.Status),
+	}
+}
+
+func ToReservationDTO(r Reservation) ReservationDTO {
+	return ReservationDTO{
+		ID:         r.ID,
+		RoomID:     r.RoomID,
+		DateFrom:   r.DateFrom,
+		DateTo:     r.DateTo,
+		GuestCount: r.GuestCount,
+		GuestID:    r.GuestID,
+		Cancelled:  r.Cancelled,
+	}
 }
