af14d28c6b67c4a1c3f25e5791beac775d2e7d3d
magley <60552482+magley@users.noreply.github.com>
2025-08-21 15:02:01 +0200

    feat: Create model and DTO for ReservationRequest

--- src/internal/model.go
+++ src/internal/model.go
@@ -1,7 +1,37 @@
 package internal
 
+import "time"
+
+type ReservationRequestStatus string
+
+const (
+	Pending  ReservationRequestStatus = "pending"
+	Accepted ReservationRequestStatus = "accepted"
+	Rejected ReservationRequestStatus = "rejected"
+)
+
+type ReservationRequest struct {
+	ID                 uint                     `gorm:"primaryKey"`
+	RoomID             uint                     `gorm:"not null"`
+	RoomAvailabilityID uint                     `gorm:"not null"`
+	RoomPriceID        uint                     `gorm:"not null"`
+	DateFrom           time.Time                `gorm:"not null"` // Including year
+	DateTo             time.Time                `gorm:"not null"` // Including year
+	GuestCount         uint                     `gorm:"not null"`
+	GuestID            uint                     `gorm:"not null"` // User who made the request
+	Status             ReservationRequestStatus `gorm:"not null"`
+	Cost               uint                     `gorm:"not null"` // Computed field
+}
+
 type Reservation struct {
-	ID      uint `gorm:"primaryKey"`
-	RoomID  uint `gorm:"not null"`
-	GuestID uint `gorm:"not null"`
+	ID                 uint      `gorm:"primaryKey"`
+	RoomID             uint      `gorm:"not null"`
+	RoomAvailabilityID uint      `gorm:"not null"`
+	RoomPriceID        uint      `gorm:"not null"`
+	GuestID            uint      `gorm:"not null"` // User who made the request
+	DateFrom           time.Time `gorm:"not null"` // Including year
+	DateTo             time.Time `gorm:"not null"` // Including year
+	GuestCount         uint      `gorm:"not null"`
+	Cancelled          bool      `gorm:"not null"`
+	Cost               uint      `gorm:"not null"` // Computed field
 }
