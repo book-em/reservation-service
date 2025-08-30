a4066f94ca83658fa54c104c9f3fae5869d2f38e
magley <60552482+magley@users.noreply.github.com>
2025-08-30 16:53:32 +0200

    feat: Add API method for checking if reservation exists

--- src/internal/handler.go
+++ src/internal/handler.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,6 +20,8 @@ func (r *Route) Route(rg *gin.RouterGroup) {
 	rg.GET("/req/user", r.handler.findPendingRequestsByGuest)
 	rg.GET("/req/room/:id", r.handler.findPendingRequestsByRoom)
 	rg.DELETE("/req/:id", r.handler.deleteRequestByGuest)
+
+	rg.GET("/room/:id/availability", r.handler.checkAvailability)
 }
 
 type Handler struct{ service Service }
@@ -149,4 +152,35 @@ func (h *Handler) deleteRequestByGuest(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusNoContent, nil)
+}
+
+func (h *Handler) checkAvailability(ctx *gin.Context) {
+	roomID, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		AbortError(ctx, ErrBadRequest)
+		return
+	}
+
+	fromStr := ctx.Query("from")
+	toStr := ctx.Query("to")
+
+	from, err := time.Parse("2006-01-02", fromStr)
+	if err != nil {
+		AbortError(ctx, ErrBadRequestCustom("invalid 'from' date format"))
+		return
+	}
+
+	to, err := time.Parse("2006-01-02", toStr)
+	if err != nil {
+		AbortError(ctx, ErrBadRequestCustom("invalid 'to' date format"))
+		return
+	}
+
+	available, err := h.service.AreThereNoReservationsOnDays(uint(roomID), from, to)
+	if err != nil {
+		AbortError(ctx, err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"available": available})
 }
