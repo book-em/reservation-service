57244bafa8e47c862dc00c6104fda91986e36d60
magley <60552482+magley@users.noreply.github.com>
2025-09-28 16:44:00 +0200

    feat: Add tracing to API and service layers

--- src/internal/handler.go
+++ src/internal/handler.go
@@ -2,7 +2,6 @@ package internal
 
 import (
 	"bookem-reservation-service/util"
-	"log"
 	"net/http"
 	"strconv"
 	"time"
@@ -28,31 +27,39 @@ type Handler struct{ service Service }
 func NewHandler(s Service) Handler { return Handler{s} }
 
 func (h *Handler) createReservationRequest(ctx *gin.Context) {
+	util.TEL.Push(ctx.Request.Context(), "create-reservation-request-api")
+	defer util.TEL.Pop()
+
 	jwtString, err := util.GetJwtString(ctx)
 	if err != nil {
+		util.TEL.Event("failed fetching JWT", err)
 		AbortError(ctx, ErrUnauthenticated)
 		return
 	}
 
 	jwt, err := util.GetJwt(ctx)
 	if err != nil {
+		util.TEL.Event("failed fetching JWT", err)
 		AbortError(ctx, ErrUnauthenticated)
 		return
 	}
 
 	if jwt.Role != util.Guest {
+		util.TEL.Eventf("user is not guest (role=%s)", nil, jwt.Role)
 		AbortError(ctx, ErrUnauthorized)
 		return
 	}
 
 	var dto CreateReservationRequestDTO
 	if err := ctx.ShouldBindJSON(&dto); err != nil {
+		util.TEL.Event("failed binding JSON", err)
 		AbortError(ctx, err)
 		return
 	}
 
 	reservation, err := h.service.CreateRequest(util.TEL.Ctx(), AuthContext{CallerID: jwt.ID, JWT: jwtString}, dto)
 	if err != nil {
+		util.TEL.Event("failed creating reservation request", err)
 		AbortError(ctx, err)
 		return
 	}
@@ -61,24 +68,30 @@ func (h *Handler) createReservationRequest(ctx *gin.Context) {
 }
 
 func (h *Handler) findPendingRequestsByGuest(ctx *gin.Context) {
-	log.Printf("findPendingRequestsByGuest called")
+	util.TEL.Push(ctx.Request.Context(), "find-pending-requests-by-guest-api")
+	defer util.TEL.Pop()
 
 	jwt, err := util.GetJwt(ctx)
 	if err != nil {
+		util.TEL.Event("failed fetching JWT", err)
 		AbortError(ctx, ErrUnauthenticated)
 		return
 	}
 
 	if jwt.Role != util.Guest {
+		util.TEL.Eventf("user is not guest (role=%s)", nil, jwt.Role)
 		AbortError(ctx, ErrUnauthorized)
 		return
 	}
 
 	requests, err := h.service.FindPendingRequestsByGuest(util.TEL.Ctx(), jwt.ID)
 	if err != nil {
+		util.TEL.Event("failed finding pending requests by guest", err)
 		AbortError(ctx, err)
 		return
 	}
+
+	util.TEL.Event("building response", err)
 
 	result := make([]ReservationRequestDTO, 0)
 	for _, req := range requests {
@@ -89,31 +102,37 @@ func (h *Handler) findPendingRequestsByGuest(ctx *gin.Context) {
 }
 
 func (h *Handler) findPendingRequestsByRoom(ctx *gin.Context) {
-	log.Printf("findPendingRequestsByRoom called")
+	util.TEL.Push(ctx.Request.Context(), "find-pending-requests-by-room-api")
+	defer util.TEL.Pop()
 
 	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		log.Printf("Could not parse ID %s: %s", ctx.Param("id"), err.Error())
+		util.TEL.Eventf("could not parse request param id %s", err, ctx.Param("id"))
 		AbortError(ctx, ErrBadRequest)
 		return
 	}
 
 	jwt, err := util.GetJwt(ctx)
 	if err != nil {
+		util.TEL.Event("failed fetching JWT", err)
 		AbortError(ctx, ErrUnauthenticated)
 		return
 	}
 
 	if jwt.Role != util.Host {
+		util.TEL.Eventf("user is not host (role=%s)", nil, jwt.Role)
 		AbortError(ctx, ErrUnauthorized)
 		return
 	}
 
 	requests, err := h.service.FindPendingRequestsByRoom(util.TEL.Ctx(), jwt.ID, uint(id))
 	if err != nil {
+		util.TEL.Event("failed finding pending requests by room", err)
 		AbortError(ctx, err)
 		return
 	}
+
+	util.TEL.Event("building response", err)
 
 	result := make([]ReservationRequestDTO, 0)
 	for _, req := range requests {
@@ -124,28 +143,32 @@ func (h *Handler) findPendingRequestsByRoom(ctx *gin.Context) {
 }
 
 func (h *Handler) deleteRequestByGuest(ctx *gin.Context) {
-	log.Printf("deleteRequestByGuest called")
+	util.TEL.Push(ctx.Request.Context(), "delete-request-by-guest-api")
+	defer util.TEL.Pop()
 
 	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		log.Printf("Could not parse ID %s: %s", ctx.Param("id"), err.Error())
+		util.TEL.Eventf("could not parse request param id %s", err, ctx.Param("id"))
 		AbortError(ctx, ErrBadRequest)
 		return
 	}
 
 	jwt, err := util.GetJwt(ctx)
 	if err != nil {
+		util.TEL.Event("failed fetching JWT", err)
 		AbortError(ctx, ErrUnauthenticated)
 		return
 	}
 
 	if jwt.Role != util.Guest {
+		util.TEL.Eventf("user is not host (role=%s)", nil, jwt.Role)
 		AbortError(ctx, ErrUnauthorized)
 		return
 	}
 
 	err = h.service.DeleteRequest(util.TEL.Ctx(), jwt.ID, uint(id))
 	if err != nil {
+		util.TEL.Event("failed deleting request by guest", err)
 		AbortError(ctx, err)
 		return
 	}
@@ -154,8 +177,12 @@ func (h *Handler) deleteRequestByGuest(ctx *gin.Context) {
 }
 
 func (h *Handler) checkAvailability(ctx *gin.Context) {
+	util.TEL.Push(ctx.Request.Context(), "check-availability-api")
+	defer util.TEL.Pop()
+
 	roomID, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
+		util.TEL.Eventf("could not parse request param id %s", err, ctx.Param("id"))
 		AbortError(ctx, ErrBadRequest)
 		return
 	}
@@ -165,18 +192,21 @@ func (h *Handler) checkAvailability(ctx *gin.Context) {
 
 	from, err := time.Parse("2006-01-02", fromStr)
 	if err != nil {
+		util.TEL.Eventf("invalid 'from' date format (should be YYYY-MM-DD, got %s)", err, from)
 		AbortError(ctx, ErrBadRequestCustom("invalid 'from' date format"))
 		return
 	}
 
 	to, err := time.Parse("2006-01-02", toStr)
 	if err != nil {
+		util.TEL.Eventf("invalid 'to' date format (should be YYYY-MM-DD, got %s)", err, from)
 		AbortError(ctx, ErrBadRequestCustom("invalid 'to' date format"))
 		return
 	}
 
 	available, err := h.service.AreThereReservationsOnDays(util.TEL.Ctx(), uint(roomID), from, to)
 	if err != nil {
+		util.TEL.Event("failed check if room has reservation in a date range", err)
 		AbortError(ctx, err)
 		return
 	}
