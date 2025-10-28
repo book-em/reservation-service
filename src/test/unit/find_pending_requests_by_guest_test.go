88614e996c792acfb8f9e1b21cd5d4fbee040582
Vasilije <vasilijezzzz@gmail.com>
2025-10-28 15:52:34 +0100

    feat: Filter out `ReservationRequests` from deleted `Rooms`

--- src/test/unit/find_pending_requests_by_guest_test.go
+++ src/test/unit/find_pending_requests_by_guest_test.go
@@ -10,20 +10,33 @@ import (
 )
 
 func Test_FindPendingRequestsByGuest_Success(t *testing.T) {
-	svc, repo, userClient, _, _ := CreateTestRoomService()
+	svc, repo, userClient, roomClient, _ := CreateTestRoomService()
+
+	room1Val := *DefaultRoom
+	room1 := &room1Val
+	room1.ID = 1
+	room1.Deleted = true
+
+	room2Val := *DefaultRoom
+	room2 := &room2Val
+	room2.ID = 2
+	room2.Deleted = false
 
 	userClient.On("FindById", context.Background(), uint(1)).Return(DefaultUser_Guest, nil)
+	roomClient.On("FindById", context.Background(), room1.ID).Return(room1, nil).Once()
+	roomClient.On("FindById", context.Background(), room2.ID).Return(room2, nil).Once()
 
 	expected := []internal.ReservationRequest{
-		{ID: 1, RoomID: 1, GuestID: 1, Status: internal.Pending},
-		{ID: 2, RoomID: 2, GuestID: 1, Status: internal.Pending},
+		{ID: 1, RoomID: room1.ID, GuestID: 1, Status: internal.Pending},
+		{ID: 2, RoomID: room2.ID, GuestID: 1, Status: internal.Pending},
 	}
 	repo.On("FindPendingRequestsByGuestID", uint(1)).Return(expected, nil)
 
 	result, err := svc.FindPendingRequestsByGuest(context.Background(), 1)
 
 	assert.NoError(t, err)
-	assert.Equal(t, expected, result)
+	assert.Equal(t, 1, len(result))
+	assert.Equal(t, room2.ID, result[0].ID)
 	repo.AssertCalled(t, "FindPendingRequestsByGuestID", uint(1))
 }
 
